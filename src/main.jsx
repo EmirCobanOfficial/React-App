b258b1832a50969722d5d47f7b9497d7fc705e69
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-05-28 16:32:28 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,53 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+function App() {
+  return (
+    <>
+      <Header />
+      <MovieList />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <div id="Header">
+      <h1>Header</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam ad, odit
+        veniam repudiandae deleniti ratione tenetur quam eos vitae inventore
+        praesentium cupiditate! Non magnam esse repellendus, aspernatur
+        distinctio nisi dolorum!
+      </p>
+    </div>
+  );
+}
+
+function MovieList() {
+  return (
+    <div>
+      <h2>Movie List</h2>
+      <div id="movie-list">
+        <Movie />
+        <Movie />
+        <Movie />
+      </div>
+    </div>
+  );
+}
+
+function Movie() {
+  return (
+    <div className="movie">
+      <h3>Movie</h3>
+      <p>Film açıklaması</p>
+    </div>
+  );
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>React App</h1>
+    <App />
   </StrictMode>
 );
