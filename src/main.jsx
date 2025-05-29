c0126c2470fbb5a6e840707ce930073606a05bff
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-05-29 11:12:49 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,14 @@ function App() {
 }
 
 function Header() {
+  const headerStyles = {
+    color: "red",
+    fontSize: "30px",
+    textTransform: "uppercase",
+  };
   return (
     <div id="Header">
-      <h1>Header</h1>
+      <h1 style={headerStyles}>Header</h1>
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam ad, odit
         veniam repudiandae deleniti ratione tenetur quam eos vitae inventore
@@ -33,6 +38,7 @@ function MovieList() {
       description:
         "Kaptan Amerika: Cesur Yeni Dünya, Kaptan Amerika'nın maceralarını konu ediyor. Amerika Birleşik Devletleri'nin yeni Başkanı Thaddeus Ross ile tanıştıktan kısa bir süre sonra Kaptan Amerika olarak da bilinen Sam Wilson, kendisini devasa bir uluslararası olayın tam ortasında bulur. Operasyonun gerçek dehası tüm dünyayı ateşe vermeden önce Sam Wilson zamana karşı amansız bir savaşa girişir",
       is_active: true,
+      is_new: true,
     },
     {
       image: "2.jpg",
@@ -40,6 +46,7 @@ function MovieList() {
       description:
         "Gündüzleri lüks bir otelde çalışan sıradan görevlilerdir: valeler, garsonlar, barmenler... Ancak geceleri, zengin müşterileri gözlerine kestirip yolda soydukları usta araba hırsızlarına dönüşürler. Büyük bir soygun planı yaparlarken, otelin yöneticisi onların peşine acımasız bir tetikçi salar. Tehlike giderek yaklaşırken, Nora, Zoe, Steve ve Prestance hayatlarının en büyük vurgununu gerçekleştirebilecekler mi?",
       is_active: true,
+      is_new: true,
     },
     {
       image: "3.jpg",
@@ -47,6 +54,7 @@ function MovieList() {
       description:
         "War stories about family, ethics and honor include the true story of two U.S. Marines who in a span of six seconds, must stand their ground to stop a suicide truck bomb, a Navy Corpsman who attempts to hold on to his humanity, and a WW2 soldier who gets separated from his squad and is forced to re-evaluate his code.",
       is_active: true,
+      is_new: true,
     },
     {
       image: "4.jpg",
@@ -54,12 +62,13 @@ function MovieList() {
       description:
         "“Moana 2”, Moana ve Maui'yi üç yıl sonra, alışılmadık denizcilerden oluşan bir ekiple birlikte geniş kapsamlı yeni bir yolculuk için yeniden bir araya getiriyor. Yol gösterici atalarından beklenmedik bir çağrı alan Moana, daha önce karşılaşmadığı bir macera için Okyanusya'nın uzak denizlerine, tehlikeli ve uzun süredir bilinmeyen sularına yolculuk etmek zorunda. Yönetmenliğini David Derrick Jr., Jason Hand ve Dana Ledoux Miller'ın, yapımcılığını Christina Chen ve Yvett Merino'nun üstlendiği “Moana 2 ”nin müzikleri Grammy® ödüllü Abigail Barlow ve Emily Bear, Grammy adayı Opetaia Foa ile, üç kez Grammy kazanan Mark Mancina'ya ait.",
       is_active: true,
+      is_new: false,
     },
   ];
 
   return (
     <div>
-      <h2>Movie List</h2>
+      <h2 className="title">Movie List</h2>
       {movie_list.filter((m) => m.is_active).length == 0 ? (
         <div>Film bulunamadı</div>
       ) : (
@@ -79,7 +88,9 @@ function Movie({ movieObj }) {
       {movieObj.is_active && (
         <div className="movie">
           <img src={"/img/" + movieObj.image} alt="" />
-          <h3>{movieObj.title}</h3>
+          <h3 className={`f30 red ${movieObj.is_new ? "new" : ""}`}>
+            {movieObj.title}
+          </h3>
           <p>{movieObj.description}</p>
         </div>
       )}
